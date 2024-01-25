d5b363c9b4bfd4a550aecc533330b48a126c0f5c
Ruth Klocker <ruth.klocker@gmail.com>
2024-01-25 19:38:48 +0100

    Created printing_grid and ship_placing function

--- battleship.py
+++ battleship.py
@@ -1,41 +1,65 @@
-player_ships = []
+import random
 
 def creating_grid():
-    while True:
-        length_of_grid_input = input("How big is the side of the grid? ")
+    while True:    
         try:
-            length_of_grid_local = int(length_of_grid_input)
-            if length_of_grid_local <= 0:
+            length_of_grid_input = int(input("How big is the side of the grid? "))
+            if length_of_grid_input <= 0:
                 print("Grid size must be at least 1.")
-            elif length_of_grid_local > 100:
+            elif length_of_grid_input > 100:
                 print("Maximal size of the grid is 100.")
             else:
                 break
         except ValueError:
             print("Invalid input. Please enter an integer.")
+    grid_local = [["." for x in range(length_of_grid_input)] for y in range(length_of_grid_input)]
+    return grid_local, length_of_grid_input
 
-    return [["." for x in range(length_of_grid_local)] for y in range(length_of_grid_local)]
-        
+def printing_grid(grid_local):
+    for i in grid_local:
+            print(i)
+    return grid_local
+
 def defining_number_ships():
     max_ships = int((len(grid)**2)/2)
-    print(max_ships)
     while True:
-        number_ships_input = (input("Enter the number of ships: "))
         try:
-            number_ships_local = int(number_ships_input)
-            if number_ships_local <= 0 or number_ships_local > max_ships:
+            number_ships_input = int(input("Enter the number of ships: "))
+            if number_ships_input <= 0 or number_ships_input > max_ships:
                 print("The number of ships must be at least 1, but cannot be more than " + str(max_ships) + ". ")
             else:
-                return number_ships_local
+                return number_ships_input
         except ValueError:
             print("Invalid input. Please enter an integer.")
 
-grid = creating_grid()
-print(grid)
-number_ships = defining_number_ships()
-
-def ship_placing_function():
+def ship_placing_function(grid, number_ships, length_of_grid):
+    grid_local = grid
+    player_ships = []
     for i in range(1, number_ships + 1):
-        player_ship = input(f"Please enter the coordinates for your ship {i} in a (x/y) format: ")
-        player_ships.append(player_ship)
-    return player_ships
+        while True:
+            try:
+                player_ship_x_input = int(input(f"Please enter the x coordinate for your ship {i}: "))
+                player_ship_y_input = int(input(f"Please enter the y coordinate for your ship {i}: "))
+            except ValueError:
+                print("Please enter an integer.")
+            if player_ship_x_input < 1 or player_ship_y_input < 1 or player_ship_x_input > length_of_grid or player_ship_y_input > length_of_grid:
+                print(f"One of the coordinates for ship {i} is out of range. The coordinate has to be between 0 and " + str(length_of_grid) + ".")
+            else:
+                print(grid_local[player_ship_x_input - 1][player_ship_y_input -1 ])
+                if grid_local[player_ship_x_input - 1][player_ship_y_input -1 ] == ".":
+                    grid_local[player_ship_x_input - 1][player_ship_y_input - 1] = "x"
+                    break
+                else:
+                    print("Field already occupied.") 
+            
+        player_ships.append([player_ship_x_input - 1,player_ship_y_input - 1])
+    return player_ships, grid_local
+
+
+
+grid, length_of_grid = creating_grid()
+printing_grid(grid)
+number_ships = defining_number_ships()
+position_player_ships, grid_with_player_ships = ship_placing_function(grid, number_ships, length_of_grid)
+print(position_player_ships)
+printing_grid(grid_with_player_ships)
